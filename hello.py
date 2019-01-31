1f46e95af2b60b0fb0a62ee7e95c97b533394939
Lele521 <tengyue@umail.ucsb.edu>
2019-01-30 18:17:50 -0800

    test

--- hello.py
+++ hello.py
@@ -2,12 +2,12 @@ import flask
 import firebase_admin
 from firebase_admin import db
 from flask import render_template
+from firebase_admin import credentials
 
 app = flask.Flask(__name__)
 
-firebase_admin.initialize_app(options={
-                              'databaseURL': 'https://findtheroommate.firebaseio.com'
-                              })
+cred = credentials.Certificate('findtheroommate-firebase-adminsdk-b2xem-031d66f91d.json')
+firebase_admin.initialize_app(cred)
 HELLO = db.reference('hello')
 
 #Home Page part
