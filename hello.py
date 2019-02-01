6e945d3d8b58521afd50217e4b749773c3da6f09
Jiqi Wang <38055473+yuguotian@users.noreply.github.com>
2019-02-01 13:04:31 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,3 +14,12 @@ wsgi_app = app.wsgi_app
 def hello():
     """Renders a sample page."""
     return "Hello World!"
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
