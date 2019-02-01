72faaf3d7a788d8e977b25cb118376d6c07adbc3
Jiqi Wang <38055473+yuguotian@users.noreply.github.com>
2019-02-01 13:02:33 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def hello():
+    """Renders a sample page."""
+    return "Hello World!"
