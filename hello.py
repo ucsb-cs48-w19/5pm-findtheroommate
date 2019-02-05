39fbfd6a5c33bb49171fd0293ca98c5302054ce5
Lele521 <tengyue@umail.ucsb.edu>
2019-02-04 16:46:06 -0800

    ...

--- hello.py
+++ hello.py
@@ -1,12 +1,28 @@
 import flask
 import firebase_admin
+import os
+import json
 from firebase_admin import db
 from flask import render_template
 from firebase_admin import credentials
 
 app = flask.Flask(__name__)
 
-firebase_admin.initialize_app(options={
+temp_credential = {}
+temp_credential["type"] = str(os.environ.get("type"))
+temp_credential["token_uri"] = str(os.environ.get("token_uri"))
+temp_credential["project_id"] = str(os.environ.get("project_id"))
+temp_credential["private_key_id"] = str(os.environ.get("private_key_id"))
+temp_credential["client_x509_cert_url"] = str(os.environ.get("client_x509_cert_url"))
+temp_credential["client_id"] = str(os.environ.get("client_id"))
+temp_credential["client_email"] = str(os.environ.get("client_email"))
+temp_credential["auth_uri"] = str(os.environ.get("auth_uri"))
+temp_credential["auth_provider_x509_cert_url"] = str(os.environ.get("auth_provider_x509_cert_url"))
+with open('result.json', 'w') as fp:
+    json.dump(temp_credential, fp)
+cred = credentials.Certificate('result.json')
+
+firebase_admin.initialize_app(cred,options={
     'databaseURL': "https://findtheroommate.firebaseio.com"
 })
 
