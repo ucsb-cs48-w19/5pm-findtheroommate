abcb35c0e75120b6d8fcd06c74ccea8e5c96edb7
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-02-06 14:09:16 -0800

    start playing with json

--- hello.py
+++ hello.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home", methods=['POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/post/new")
+def new_post():
+    return render_template('create_post.html', title='New Post')
+    
+
+@app.route("/success")
+def success():
+    name = request.args.get('user_name')
+    email = request.args.get('user_email')
+    gender = request.args.get('user_gender')
+    description = request.args.get('user_description')
+
+    json = {
+        'name': name,
+        'email': email,
+        'gender': gender,
+        'description': description
+    }
+    return jsonify(json)
+    #return render_template('success.html', name = name, gender=gender,email=email, description=description)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
