bfd07c06a59fe6a35b09686438b9bf830670e8a5
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-02-14 18:19:10 -0800

    MVP

--- roommate/routes.py
+++ roommate/routes.py
@@ -2,9 +2,9 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from flaskblog.models import User, Post
+from roommate import app, db, bcrypt
+from roommate.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from roommate.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flaskblog import app
+from roommate import app
 
 if __name__ == '__main__':
     app.run(debug=True)
