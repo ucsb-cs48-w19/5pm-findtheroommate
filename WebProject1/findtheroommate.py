e473d9d23cc91b31efc332163e4dae075bbcd8fe
philjin <35672816+philjin@users.noreply.github.com>
2019-02-27 14:31:22 -0800

    Update findtheroommate.py

--- WebProject1/findtheroommate.py
+++ WebProject1/findtheroommate.py
@@ -1,12 +1,2 @@
 from app import app
-import os
 
-'''if __name__ == '__main__':
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-
-    app.run(HOST, PORT)'''
