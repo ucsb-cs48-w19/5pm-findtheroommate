af7c1a93c84ce38a570e0df40ec28489b57e42f9
Jiqi Wang <38055473+yuguotian@users.noreply.github.com>
2019-03-04 20:23:20 -0800

    Update forms.py

--- WebProject1/app/forms.py
+++ WebProject1/app/forms.py
@@ -9,7 +9,7 @@ class SearchForm(FlaskForm):
 	search = StringField('')
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(),Length(min=6, max=30)])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
     
@@ -64,7 +64,7 @@ class PostForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),Length(min=6, max=30)])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
