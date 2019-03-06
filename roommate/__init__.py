ff2c3ece9e9e8cc8146c3be6b6fa2cdc95192a81
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-03-05 22:34:11 -0800

    Update __init__.py

--- roommate/__init__.py
+++ roommate/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = 'mySecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
