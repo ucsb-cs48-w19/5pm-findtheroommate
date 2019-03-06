85ad095f9f95c90ce01b83638bc5f7270f6e1ae9
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-03-05 23:07:20 -0800

    Update __init__.py

--- roommate/__init__.py
+++ roommate/__init__.py
@@ -10,7 +10,8 @@ import flask_whooshalchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+'sqlite:///' + os.path.join(basedir, 'site.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -18,11 +19,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-#app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-#app.config['MAIL_PORT'] = 587
-#app.config['MAIL_USE_TLS'] = True
-#app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-#app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER') or 'findtheroommate@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS') or 'Find_cs48'
+app.config['ADMINS'] = ['findtheroommate@gmail.com']
 
 mail = Mail(app)
 
