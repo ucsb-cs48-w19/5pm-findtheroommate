7456b66aef76b7f9fd1fbf754cb436ccec1db1f4
Yixiao (Bella) Yue <32970034+ccatbbella@users.noreply.github.com>
2019-03-05 22:34:37 -0800

    Update __init__.py

--- roommate/__init__.py
+++ roommate/__init__.py
@@ -9,7 +9,7 @@ import flask_whooshalchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = 'mySecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
