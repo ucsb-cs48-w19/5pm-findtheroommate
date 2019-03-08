52a61f06f560d09145a5448b047f34b848eda531
Lele521 <34825684+Lele521@users.noreply.github.com>
2019-03-07 17:25:34 -0800

    Update config.py

--- WebProject1/config.py
+++ WebProject1/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = #Placeholder
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,9 +10,9 @@ class Config(object):
 
     #SECURITY_EMAIL_SENDER = 'valid_email@my_domain.com'
 
-    MAIL_SERVER = 'smtp.gmail.com' #os.environ.get('MAIL_SERVER')
-    MAIL_PORT = 587 #int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = 1 #os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = 'findtheroommate@gmail.com' #os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = 'Find_cs48' #os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['findtheroommate@gmail.com']
+    MAIL_SERVER = #os.environ.get('MAIL_SERVER')
+    MAIL_PORT = #int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = #os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = #os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = #os.environ.get('MAIL_PASSWORD')
+    ADMINS = #placeholder
